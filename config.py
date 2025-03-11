f5edadb1b7f4ef546b8a2daa89b6a9591be68a68
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2025-03-11 23:59:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7441315027:AAEfcoSuD_Cz7p_OoV0oVYe4-5irG5S6G2M")
+    API_ID = int(os.environ.get("API_ID", "20589413"))
+    API_HASH = os.environ.get("API_HASH", "654d559a9a91daeecd9e760fc73e6766")
+    AUTH_USER = os.environ.get('AUTH_USERS', '6514361814').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://drm-api-five.vercel.app"
-    CREDIT = "Iɴᴅɪᴀɴ ✗ Wᴀʀʀɪᴏʀꜱ" #Here You Can Change with Your Name  or any custom name or title you prefer
+    HOST = "https://uploader-drm-6a0cf44af499.herokuapp.com/"
+    CREDIT = "sheffyssamra" #Here You Can Change with Your Name  or any custom name or title you prefer
